6675e63eb2287c070a78800160ffb42796b83dea
IDKKKk123 <158230548+IDKKKk123@users.noreply.github.com>
2024-06-18 14:26:49 +0300

    new file

---
+++ js/app_BACKUP_2021.js
@@ -0,0 +1,13 @@
+<<<<<<< HEAD
+(async () => {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(position => {
+            const coordsEl = document.querySelector('#user-coords');
+            coordsEl.textContent = `${position.coords.latitude}, ${position.coords.longitude}`
+            coordsEl.parentElement.classList.remove('invisible');
+        });
+    }
+})();
+=======
+
+>>>>>>> d9e2a35cb9f33357f90db407f30545ae19da584a
